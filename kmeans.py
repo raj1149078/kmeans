7efb063e1df4f6616cf98072c3f3df8745a1b37c
raj1149078 <75312209+raj1149078@users.noreply.github.com>
2021-03-14 17:07:25 -0500

    Update and rename kmeans to kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("points.txt")
 
